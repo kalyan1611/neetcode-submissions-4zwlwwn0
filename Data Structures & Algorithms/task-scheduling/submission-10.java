5372e83a1fb0c4c2832248d5c9860df8be96d3b7
kalyan.cherukuru <30562592+kalyan1611@users.noreply.github.com>
2026-04-21 16:35:02 +0530

    Add: task-scheduling - submission-10

---
+++ Data Structures & Algorithms/task-scheduling/submission-10.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+
+        var scheduler = new PriorityQueue<Integer>(Comparator.reverseOrder());
+
+        var map = new HashMap<Character, Integer>();
+
+        var queue = new ArrayDeque<int[]>();
+
+        for (char task : tasks) {
+            map.put(task, map.getOrDefault(task, 0) + 1);
+        }
+
+        for (var entry : map.entrySet()) {
+            scheduler.offer(entry.getValue());
+        }
+
+        int t = 0;
+
+        while (!scheduler.isEmpty() || !queue.isEmpty()) {
+            if (!queue.isEmpty() && queue.peek()[1] == t) {
+                scheduler.offer(queue.poll()[0]);
+            }
+            
+            if (!scheduler.isEmpty()) {
+                int cur = scheduler.poll();
+                if (cur > 1) {
+                    queue.add(new int[]{cur - 1, t + n + 1});
+                }
+            }
+            t++;
+        }
+
+        return t;
+    }
+}
